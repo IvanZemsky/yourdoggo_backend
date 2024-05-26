69d55dbe629fb147cb95568cf4b66031ef773271
IvanZemsky <i.ivanzemsky2005@gmail.com>
2024-05-27 01:39:13 +0300

    params for gallery endpoint

--- src/services/galleryService.js
+++ src/services/galleryService.js
@@ -5,6 +5,14 @@ class GalleryService {
       const query = {};
       const limit = +params.limit || 0
 
+      if (params.search) {
+         const searchRegex = new RegExp(params.search, 'i');
+         query.$or = [
+            { title: searchRegex },
+            { tags: searchRegex }
+         ];
+      }
+
       if (params.userLogin) {
          const images = await GalleryImg.find(query).populate(
             "userId",
@@ -19,7 +27,7 @@ class GalleryService {
          });
          return transformedImages;
       } else {
-         const images = await GalleryImg.find(query);
+         const images = await GalleryImg.find(query).limit(limit);
          return images;
       }
    }
